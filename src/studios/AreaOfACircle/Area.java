498a8ef9617628e077849beb1ad7f4cc2325adb5
Nathan-422 <natiouswright@gmail.com>
2023-01-05 19:54:54 -0600

    Studio complete

---
+++ src/studios/AreaOfACircle/Area.java
@@ -0,0 +1,52 @@
+/*
+Authors: Alena, Gabbie, Nathan
+Class: lc_101
+Section: September 2022
+Instruction: Carrie Jones
+ */
+
+package studios.AreaOfACircle;
+
+import java.util.Scanner;
+
+public class Area {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        Double r = 1.0;
+
+        while (true) {
+
+            System.out.println("Give a radius: ");
+            String userInput = input.nextLine();
+
+            if (userInput.equals("")) {
+                System.out.println("Input must not be empty");
+                continue;
+            }
+
+            // try catch
+            try {
+                r = Double.parseDouble(userInput);
+            } catch (Exception e) {
+                System.out.println("Input must be a number.");
+                continue;
+            }
+
+            if (r <= 0.0) {
+                System.out.println("Input must be positive.");
+                continue;
+            }
+
+            break;
+
+        }
+
+        double area = Circle.getArea(r);
+
+        System.out.println("The area of a circle of radius " + r + " is: " + Math.round(area * 1000) / 1000f);
+    }
+
+}
---
+++ src/studios/AreaOfACircle/Circle.java
@@ -0,0 +1,9 @@
+package studios.AreaOfACircle;
+
+public class Circle {
+
+    public static Double getArea(double radius) {
+        return 3.14 * radius * radius;
+    }
+
+}
