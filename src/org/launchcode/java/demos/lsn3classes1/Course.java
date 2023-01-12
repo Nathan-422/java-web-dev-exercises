e3435505d6cfc77454a780072a0705b2e1f43dfa
Nathan-422 <natiouswright@gmail.com>
2023-01-12 15:47:13 -0600

    Initial exercise 3 materials complete

--- src/org/launchcode/java/demos/lsn3classes1/Course.java
+++ src/org/launchcode/java/demos/lsn3classes1/Course.java
@@ -1,2 +1,36 @@
-package org.launchcode.java.demos.lsn3classes1;public class Course {
+package org.launchcode.java.demos.lsn3classes1;
+
+import java.util.ArrayList;
+
+public class Course {
+
+    private String name;
+    private String section;
+    private ArrayList<Teacher> instructors;
+    private ArrayList<Student> students;
+
+    public Course(String name, String section, Teacher instructor) {
+        this.name = name;
+        this.section = section;
+        this.instructors = new ArrayList<>();
+        this.students = new ArrayList<>();
+
+        this.instructors.add(instructor);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSection() {
+        return section;
+    }
+
+    public ArrayList<Teacher> getInstructors() {
+        return new ArrayList<>(instructors);
+    }
+
+    public ArrayList<Student> getStudents() {
+        return new ArrayList<Student>(students);
+    }
 }
--- src/org/launchcode/java/demos/lsn3classes1/Student.java
+++ src/org/launchcode/java/demos/lsn3classes1/Student.java
@@ -5,9 +5,65 @@ package org.launchcode.java.demos.lsn3classes1;
 
 public class Student {
 
-    private String name;
+    private String firstName;
+    private String lastName;
     private int studentId;
     private int numberOfCredits = 0;
     private double gpa = 0.0;
 
+    public Student(String firstName, String lastName, int studentId, int numberOfCredits, double gpa) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.studentId = studentId;
+        this.numberOfCredits = numberOfCredits;
+        this.gpa = gpa;
+    }
+
+    public Student (String firstName, String lastName, int studentId) {
+        this(firstName, lastName, studentId, 0, 0.0f);
+    }
+
+    public String getName() {
+        return firstName + " " + lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public int getNumberOfCredits() {
+        return numberOfCredits;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    private void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public void setNumberOfCredits(int numberOfCredits) {
+        this.numberOfCredits = numberOfCredits;
+    }
+
+    private void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
 }
