ca5d7bcfcb1cb9794f0215e81a9a9a75ab2a9181
Nathan-422 <natiouswright@gmail.com>
2023-01-30 12:16:24 -0600

    Added main file to lsn6. Init technology pkg

---
+++ src/org/launchcode/java/demos/lsn6inheritance/Main.java
@@ -0,0 +1,15 @@
+package org.launchcode.java.demos.lsn6inheritance;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        HouseCat ruri = new HouseCat("Ruri", 11.4);
+
+        System.out.println(ruri.purr());
+
+        System.out.println(ruri.getFamily());
+
+    }
+
+}
