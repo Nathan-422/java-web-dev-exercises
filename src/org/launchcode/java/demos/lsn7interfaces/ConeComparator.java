478358f9417a9454e8050dd05cc8305a475dc02f
Nathan <natiouswright@gmail.com>
2023-02-02 16:01:24 -0600

    ConeComparator class working

--- src/org/launchcode/java/demos/lsn7interfaces/ConeComparator.java
+++ src/org/launchcode/java/demos/lsn7interfaces/ConeComparator.java
@@ -1,2 +1,19 @@
-package org.launchcode.java.demos.lsn7interfaces;public class ConeComparator {
+package org.launchcode.java.demos.lsn7interfaces;
+
+import java.util.Comparator;
+
+public class ConeComparator implements Comparator<Cone> {
+
+
+    @Override
+    public int compare(Cone cone, Cone t1) {
+
+        double result = cone.getCost() - t1.getCost();
+        if (result < 0) {
+            return -1;
+        } else if (result > 0) {
+            return 1;
+        }
+        return 0;
+    }
 }
