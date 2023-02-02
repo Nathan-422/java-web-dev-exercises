f405388de5c540b09e7ecbc1e619cfa0ecd90468
Nathan <natiouswright@gmail.com>
2023-02-02 15:40:02 -0600

    Flavor comparitor implemented

--- src/org/launchcode/java/demos/lsn7interfaces/FlavorComparator.java
+++ src/org/launchcode/java/demos/lsn7interfaces/FlavorComparator.java
@@ -1,2 +1,11 @@
-package org.launchcode.java.demos.lsn7interfaces;public class FlavorComparator {
+package org.launchcode.java.demos.lsn7interfaces;
+
+import java.util.Comparator;
+
+public class FlavorComparator implements Comparator<Flavor> {
+
+    @Override
+    public int compare(Flavor flavor, Flavor t1) {
+        return flavor.getName().compareTo(t1.getName());
+    }
 }
